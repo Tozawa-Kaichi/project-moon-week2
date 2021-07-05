1677f0efc1982d9e9fb065a55addabdcd8214f6d
戸澤海智 <st37071556@icloud.com>
2021-07-05 10:39:52 +0900

    月曜日 ２週目

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 敵の基本的な機能を制御するコンポーネント
+/// </summary>
+public class EnemyController : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerBulletController>())  // 衝突相手が 弾 だったら
+        {
+            Destroy(collision.gameObject);  // 弾のオブジェクトを破棄する
+            Destroy(this.gameObject);       // そして自分も破棄する
+            /* ============
+             * 注意: このタイミングで音を鳴らす時は注意が必要です。          
+             * ============ */
+        }
+    }
+}
